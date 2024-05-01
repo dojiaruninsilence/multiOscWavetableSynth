0e299c820b731d3c971b3926c057850054a54f9f
Zubin Falberg <the.z.creations@gmail.com>
2024-05-01 06:59:55 -0600

    defined basic audioBufferQueue class:

--- Source/Audio/AudioBufferQueue.cpp
+++ Source/Audio/AudioBufferQueue.cpp
@@ -9,3 +9,35 @@
 */
 
 #include "AudioBufferQueue.h"
+
+template <typename SampleType>
+void AudioBufferQueue<SampleType>::push(const SampleType* dataToPush, size_t numSamples) {
+    jassert(numSamples <= bufferSize);
+
+    int start1, size1, start2, size2;
+    abstractFifo.prepareToWrite(1, start1, size1, start2, size2);
+
+    jassert(size1 <= 1);
+    jassert(size2 == 0);
+
+    if (size1 > 0) {
+        juce::FloatVectorOperations::copy(buffers[(size_t)start1].data(), dataToPush, (int)juce::jmin(bufferSize, numSamples));
+    }
+
+    abstractFifo.finishedWrite(size1);
+}
+
+template <typename SampleType>
+void AudioBufferQueue<SampleType>::pop(SampleType* outputBuffer) {
+    int start1, size1, start2, size2;
+    abstractFifo.prepareToRead(1, start1, size1, start2, start2);
+
+    jassert(size1 <= 1);
+    jassert(size2 == 0);
+
+    if (size1 > 0) {
+        juce::FloatVectorOperations::copy(outputBuffer, buffers[(size_t)start1].data(), (int)bufferSize);
+    }
+
+    abstractFifo.finishedRead(size1);
+}
--- Source/Audio/AudioBufferQueue.h
+++ Source/Audio/AudioBufferQueue.h
@@ -9,3 +9,21 @@
 */
 
 #pragma once
+
+#include <JuceHeader.h>
+
+template <typename SampleType>
+class AudioBufferQueue
+{
+public:
+    static constexpr size_t order = 9;
+    static constexpr size_t bufferSize = 1U << order;
+    static constexpr size_t numBuffers = 5;
+
+    void push(const SampleType* dataToPush, size_t numSamples);
+    void pop(SampleType* outputBuffer);
+
+private:
+    juce::AbstractFifo abstractFifo{ numBuffers };
+    std::array<std::array<SampleType, bufferSize>, numBuffers> buffers;
+};
