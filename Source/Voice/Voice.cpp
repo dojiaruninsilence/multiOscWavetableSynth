3c2f844ab17e6b12cdb8d023af4abf25a6e4a878
Zubin Falberg <the.z.creations@gmail.com>
2024-05-01 05:57:09 -0600

    defined basic voice class

--- Source/Voice/Voice.cpp
+++ Source/Voice/Voice.cpp
@@ -9,3 +9,51 @@
 */
 
 #include "Voice.h"
+
+Voice::Voice() {
+    auto& masterGain = processorChain.get<masterGainIndex>();
+    masterGain.setGainLinear(0.7f);
+}
+
+void Voice::prepare(const juce::dsp::ProcessSpec& spec) {
+    tempBlock = juce::dsp::AudioBlock<float>(heapBlock, spec.numChannels, spec.maximumBlockSize);
+    processorChain.prepare(spec);
+}
+
+void Voice::noteStarted() {
+    auto velocity = getCurrentlyPlayingNote().noteOnVelocity.asUnsignedFloat();
+    auto freqHz = (float)getCurrentlyPlayingNote().getFrequencyInHertz();
+
+    processorChain.get<osc1Index>().setFrequency(freqHz, true);
+    processorChain.get<osc1Index>().setLevel(velocity);
+}
+
+void Voice::notePitchbendChanged() {
+    auto freqHz = (float)getCurrentlyPlayingNote().getFrequencyInHertz();
+    processorChain.get<osc1Index>().setFrequency(freqHz);
+}
+
+void Voice::noteStopped(bool) {
+    clearCurrentNote();
+}
+
+void Voice::notePressureChanged() {
+
+}
+
+void Voice::noteTimbreChanged() {
+
+}
+
+void Voice::noteKeyStateChanged() {
+
+}
+
+void Voice::renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) {
+    auto block = tempBlock.getSubBlock(0, (size_t)numSamples);
+    block.clear();
+    juce::dsp::ProcessContextReplacing<float>context(block);
+    processorChain.process(context);
+
+    juce::dsp::AudioBlock<float>(outputBuffer).getSubBlock((size_t)startSample, (size_t)numSamples).add(tempBlock);
+}
--- Source/Voice/Voice.h
+++ Source/Voice/Voice.h
@@ -9,3 +9,33 @@
 */
 
 #pragma once
+
+#include <JuceHeader.h>
+
+#include "../Osc/CustomOscillator.h"
+
+class Voice : public juce::MPESynthesiserVoice
+{
+public:
+    Voice();
+    void prepare(const juce::dsp::ProcessSpec& spec);
+    void noteStarted() override;
+    void notePitchbendChanged() override;
+    void noteStopped(bool) override;
+
+    void notePressureChanged() override;
+    void noteTimbreChanged() override;
+    void noteKeyStateChanged() override;
+
+    void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
+private:
+    juce::HeapBlock<char> heapBlock;
+    juce::dsp::AudioBlock<float> tempBlock;
+
+    enum {
+        osc1Index,
+        masterGainIndex
+    };
+
+    juce::dsp::ProcessorChain<CustomOscillator<float>, juce::dsp::Gain<float>> processorChain;
+};
