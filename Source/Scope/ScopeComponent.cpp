3995be0599681ccfbeb9694533ea86a14cddde4f
Zubin Falberg <the.z.creations@gmail.com>
2024-05-01 11:34:02 -0600

    defined basic scopeComponent class

--- Source/Scope/ScopeComponent.cpp
+++ Source/Scope/ScopeComponent.cpp
@@ -12,40 +12,83 @@
 #include "ScopeComponent.h"
 
 //==============================================================================
-ScopeComponent::ScopeComponent()
-{
-    // In your constructor, you should add any child components, and
-    // initialise any special settings that your component needs.
-
+template <typename SampleType>
+ScopeComponent<SampleType>::ScopeComponent(Queue& queueToUse)
+    : audioBufferQueue(queueToUse) {
+    sampleData.fill(SampleType(0));
+    setFramesPerSecond(30);
 }
 
-ScopeComponent::~ScopeComponent()
-{
+template <typename SampleType>
+ScopeComponent<SampleType>::~ScopeComponent() {
 }
 
-void ScopeComponent::paint (juce::Graphics& g)
-{
-    /* This demo code just fills the component's background and
-       draws some placeholder text to get you started.
-
-       You should replace everything in this method with your own
-       drawing code..
-    */
-
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
-
-    g.setColour (juce::Colours::grey);
-    g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
-
-    g.setColour (juce::Colours::white);
-    g.setFont (14.0f);
-    g.drawText ("ScopeComponent", getLocalBounds(),
-                juce::Justification::centred, true);   // draw some placeholder text
+template <typename SampleType>
+void ScopeComponent<SampleType>::setFramesPerSecond(int framesPerSecond) {
+    jassert(framesPerSecond > 0 && framesPerSecond < 1000);
+    startTimerHz(framesPerSecond);
 }
 
-void ScopeComponent::resized()
-{
+template <typename SampleType>
+void ScopeComponent<SampleType>::paint (juce::Graphics& g) {
+    // fill black
+    g.fillAll(juce::Colours::black); 
+    g.setColour(juce::Colours::white);
+    
+    auto area = getLocalBounds();
+    auto h = (SampleType)area.getHeight();
+    auto w = (SampleType)area.getWidth();
+
+    // oscilloscope
+    auto scopeRect = juce::Rectangle<SampleType>{ SampleType(0), SampleType(0), w, h / 2 };
+    plot(sampleData.data(), sampleData.size(), g, scopeRect, SampleType(1), h / 4);
+
+    // spectrum
+    auto spectrumRect = juce::Rectangle<SampleType>{ SampleType(0), h / 2, w, h / 2 };
+    plot(spectrumRect.data(), spectrumData.size() / 4, g, spectrumRect);
+}
+
+template <typename SampleType>
+void ScopeComponent<SampleType>::resized() {
     // This method is where you should set the bounds of any child
     // components that your component contains..
 
+}
+
+template <typename SampleType>
+void ScopeComponent<SampleType>::timerCallback() {
+    audioBufferQueue.pop(sampleData.data());
+    juce::FloatVectorOperations::copy(spectrumData.data(), sampleData.data(), (int)sampleData.size());
+
+    auto fftSize = (size_t)fft.getSize();
+
+    jassert(spectrumData.size() == 2 * fftSize);
+    windowFun.multiplyWithWindowingTable(spectrumData.data(), fftSize);
+    fft.performFrequencyOnlyForwardTransform(spectrumData.data());
+
+    static constexpr auto mindB = SampleType(-160);
+    static constexpr auto maxdB = SampleType(0);
+
+    for (auto& s : spectrumData) {
+        s = juce::jmap(juce::jlimit(mindB, maxdB, juce::Decibels::gainToDecibels(s) - juce::Decibels::gainToDecibels(SampleType(fftSize))), mindB, maxdB, SampleType(0), SampleType(1));
+    }
+
+    repaint();
+}
+
+template <typename SampleType>
+void ScopeComponent<SampleType>::plot(const SampleType* data, size_t numSamples, juce::Graphics& g, juce::Rectangle<SampleType> rect, SampleType scaler, SampleType offset) {
+    auto w = rect.getWidth();
+    auto h = rect.getHeight();
+    auto right = rect.getRight();
+
+    auto center = rect.getBottom() - offset;
+    auto gain = h * scaler;
+
+    for (size_t i = 1; i < numSamples; ++i) {
+        g.drawLine({ juce::jmap(SampleType(i - 1), SampleType(0), SampleType(numSamples - 1), SampleType(right - w), SampleType(right)),
+            center - gain * data[i - 1],
+            juce::jmap(SampleType(i), SampleType(0), SampleType(numSamples - 1), SampleType(right - w), SampleType(right)),
+            center - gain * data[i] });
+    }
 }
--- Source/Scope/ScopeComponent.h
+++ Source/Scope/ScopeComponent.h
@@ -12,18 +12,37 @@
 
 #include <JuceHeader.h>
 
+#include "../Audio/AudioBufferQueue.h"
+
 //==============================================================================
 /*
 */
-class ScopeComponent  : public juce::Component
+template <typename SampleType>
+class ScopeComponent  : public juce::Component,
+    private juce::Timer
 {
 public:
-    ScopeComponent();
+    using Queue = AudioBufferQueue<SampleType>;
+
+    ScopeComponent(Queue& queueToUse);
     ~ScopeComponent() override;
+
+    void setFramesPerSecond(int framesPerSecond);
 
     void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
+    Queue& audioBufferQueue;
+    std::array<SampleType, Queue::bufferSize> sampleData;
+
+    juce::dsp::FFT fft{ Queue::order };
+    using WindowFun = juce::dsp::WindowingFunction<SampleType>;
+    WindowFun windowFun{ (size_t)fft.getSize(), WindowFun::hann };
+    std::array<SampleType, 2 * Queue::bufferSize> spectrumData;
+
+    void timerCallback() override;
+    static void plot(const SampleType* data, size_t numSamples, juce::Graphics& g, juce::Rectangle<SampleType> rect, SampleType scaler = SampleType(1), SampleType offset = SampleType(0));
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ScopeComponent)
 };
