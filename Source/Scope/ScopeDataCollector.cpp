496571ed0ea08776d4ff1c6bfcfb118ef0d7ddb5
Zubin Falberg <the.z.creations@gmail.com>
2024-05-01 10:34:51 -0600

    defined basic scopeDataCollector class

--- Source/Scope/ScopeDataCollector.cpp
+++ Source/Scope/ScopeDataCollector.cpp
@@ -9,3 +9,40 @@
 */
 
 #include "ScopeDataCollector.h"
+
+template <typename SampleType>
+ScopeDataCollector<SampleType>::ScopeDataCollector(AudioBufferQueue<SampleType>& queueToUse)
+    : audioBufferQueue(queueToUse){
+}
+
+template <typename SampleType>
+void ScopeDataCollector<SampleType>::process(const SampleType* data, size_t numSamples) {
+    size_t index = 0;
+
+    if (state == State::waitingForTrigger) {
+        while (index++ < numSamples) {
+            auto currentSample = *data++;
+
+            if (currentSample >= triggerLevel && prevSample < triggerLevel) {
+                numCollected = 0;
+                state = State::collecting;
+                break;
+            }
+
+            prevSample = currentSample;
+        }
+    }
+
+    if (state == State::collecting) {
+        while (index++ < numSamples) {
+            buffer[numCollected++] = *data++;
+
+            if (numCollected == buffer.size()) {
+                audioBufferQueue.push(buffer.data(), buffer.size());
+                state = State::waitingForTrigger;
+                prevSample = SampleType(100);
+                break;
+            }
+        }
+    }
+}
--- Source/Scope/ScopeDataCollector.h
+++ Source/Scope/ScopeDataCollector.h
@@ -9,3 +9,22 @@
 */
 
 #pragma once
+
+#include "../Audio/AudioBufferQueue.h"
+
+template <typename SampleType>
+class ScopeDataCollector
+{
+public:
+    ScopeDataCollector(AudioBufferQueue<SampleType>& queueToUse);
+    void process(const SampleType* data, size_t numSamples);
+private:
+    AudioBufferQueue<SampleType>& audioBufferQueue;
+    std::array<SampleType, AudioBufferQueue<SampleType>::bufferSize> buffer;
+    size_t numCollected;
+    SampleType prevSample = SampleType(100);
+
+    static constexpr auto triggerLevel = SampleType(0.05);
+
+    enum class State {waitingForTrigger, collecting} state {State::waitingForTrigger};
+};
